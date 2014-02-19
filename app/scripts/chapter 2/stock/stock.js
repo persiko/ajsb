0ee5be77845b8090505dbef778475cafc458a61e
Pär Sikö <par.siko@gmail.com>
2014-02-19 20:17:52 +0100

    More examples

---
+++ app/scripts/chapter 2/stock/stock.js
@@ -0,0 +1,18 @@
+angular.module('Stock', []).controller('StockController',
+
+    function($scope) {
+
+    }
+
+).directive('stock',
+
+    function() {
+
+        return {
+            restrict: 'E',
+            template: "What the F**k"
+        }
+
+    }
+
+);
